f3d8196306fa23180bf9d4197febe2feee7fff81
zhouhuagang <37436803+zhouhuagang@users.noreply.github.com>
2018-04-24 17:28:58 +0800

    Initial commit

---
+++ djstlaTest/djstlaTest.cpp
@@ -0,0 +1,21 @@
+// djstlaTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <ostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	cout<<"hello"<<endl;
+	return 0;
+}
+
+
+#define  MAXVEX		
+#define  INFINITY 65535
+typedef int Pathmatrix[MAXVEX];
+
+
